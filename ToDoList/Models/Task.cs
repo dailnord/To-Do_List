c3d7a44e8f33ddd13d7bbdad265c1f9be95f698c
dailnord <dnorde290@gmail.com>
2025-11-22 17:19:03 -0500

    backend models

---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool IsCompleted { get; set; } 
+    }
+}
---
+++ ToDoList/Models/TaskDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().ToTable("Tasks");
+        }
+    }
+}
